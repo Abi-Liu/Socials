3abf78e3c499cf50beeefa699a1f8ddec6bb67ef
Abi Liu <abiliu018@gmail.com>
2023-02-22 20:23:34 -0800

    first commit

---
+++ controllers/comments.js
@@ -0,0 +1,49 @@
+const Comments = require('../models/Comment')
+
+module.exports = {
+    createComment: async (req, res) => {
+        try{
+            await Comments.create({
+                comment: req.body.comment,
+                likes: 0,
+                likesArr: [],
+                user: req.user.id,
+                post: req.params.id,
+            })
+        } catch (err){
+            console.error(err)
+        }
+        console.log('Comment added')
+        res.redirect(`/post/${req.params.id}`)
+    },
+    likeComment: async (req, res) => {
+        try{
+            const comment = await Comments.find({_id: req.params.id})
+            const likesArr = comment[0].likesArr
+            if(likesArr.includes(req.user.id)){
+                console.log('user already liked')
+            } else {
+                await Comments.findOneAndUpdate({_id: req.params.id},
+                {
+                    $inc: {'likes': 1},
+                    $push: {'likesArr': req.user.id}
+                })
+                console.log('like added')
+            }
+            res.redirect(`/post/${comment[0].post}`)
+        } catch(err){
+            console.error(err)
+        }
+    },
+    deleteComment: async(req, res) => {
+        try{
+            const comment = await Comments.find({_id: req.params.id})
+            const post = String(comment[0].post)
+            await Comments.remove({_id: req.params.id})
+            console.log('deleted comment')
+            res.redirect(`/post/${post}`)
+        }catch (err){
+            console.error(err)
+        }
+    }
+}
